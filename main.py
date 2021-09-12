fix gradient definition

main.py
@@ -110,15 +110,15 @@ test_loader = DataLoader(f_dataset_test, batch_size = batch_size, shuffle = True
 from tools.trainer import train
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
-model = NN(2, 2, 50, 5, batch_size=batch_size).train().to(device)
-# model.load_weights('weights/basic.pth.tar')
+model = NN(2, 2, 60, 5, batch_size=batch_size).train().to(device)
+# model.load_weights('weights/lbfgs_ckpt.pth.tar')
 
 # optimizer = torch.optim.Adam(model.parameters(), lr=1e-4, amsgrad = False)
 optimizer = torch.optim.LBFGS(model.parameters(), lr = 1.0, max_eval = 500, history_size = 50, max_iter = 500, line_search_fn = 'strong_wolfe', tolerance_grad=1e-5, tolerance_change=1.0 * np.finfo(float).eps)
 
 scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'min', .2, 500)
 
-train_loss, val_loss, lr_list = train(model, loader, test_loader, optimizer, scheduler, 6, weights_dir = './weights/lbfgs.pth.tar', type_ = 'LBFGS')
+train_loss, val_loss, lr_list = train(model, loader, test_loader, optimizer, scheduler, 50, weights_dir = './weights/lbfgs.pth.tar', type_ = 'LBFGS')
 
 plt.subplot(121)
 plt.plot(train_loss)

test.py
@@ -18,48 +18,38 @@ timesteps = list(set(data_test[:, 0]))
 batch_size = 64
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
-model = NN(2, 2, 50, 5, batch_size = batch_size).to(device)
+model = NN(2, 2, 60, 5, batch_size = batch_size).to(device)
 model.load_weights('weights/lbfgs.pth.tar')
 # model.eval()
 
 for _id, timestep in tqdm.tqdm(enumerate(timesteps)):
 
     t_ids_test = np.where(data_test[:, 0] == timestep)[0]
-    # print(t_ids_test)
-    # print('number points of points in timestep', timestep, len(t_ids_test))
 
     t_ids_train = np.where(data_train[:, 0] == timestep)[0]
-
-    # print(t_ids_train)
-
-
 
     outputs = []
     x_inputs = []
 
     t_data = data_test[t_ids_test]
-    # print('____________', t_data)
-    ## t_data = data_train[t_ids_train]
 
     for i in range(0, t_data.shape[0], batch_size):
-        # print(i)
-        #print('sizes of the data', data_test.shape, i, i+batch_size)
-        # batch = data_test[i:i+batch_size, :]
 
         batch = t_data[i:i+batch_size, ...]
-        # print('shape of batch', batch.shape)
-        if batch.shape[0] == batch_size:
-            # print('shape of batch', batch.shape, i, i+batch_size)
-            # print('input', batch[:, [3, 2]])
+        
+        if not batch.shape[0] == batch_size:
+            batch = t_data[t_data.shape[0] - batch_size:t_data.shape[0], ...]
+        # if batch.shape[0] == batch_size:
 
-            _input = torch.tensor(batch[:, [3, 2]], dtype = torch.float32, device = model.device)
-            # print('second dimension of batch', batch[:, 2])
-            # print('input', _input.shape)
-            output = model(_input)
-            # print('ouput', output.shape)
-            outputs.append(output.detach().cpu().numpy())
-            # print('output', output)
-            x_inputs.append(batch[:, 3])
+        _input = torch.tensor(batch[:, [3, 2]], dtype = torch.float32, device = model.device)
+
+        x = _input[:, 0]
+        t = _input[:, 1]
+
+        output = model(x, t)
+        outputs.append(output.detach().cpu().numpy())
+        x_inputs.append(batch[:, 3])
+
     outputs = np.array(outputs).reshape((-1, 2))
     x_inputs = np.array(x_inputs).reshape(-1)
     
@@ -68,16 +58,14 @@ for _id, timestep in tqdm.tqdm(enumerate(timesteps)):
     plt.scatter(data_train[t_ids_train, 3], data_train[t_ids_train, 4], s = 5, c = 'b')
     plt.scatter(data_train[t_ids_train, 3], data_train[t_ids_train, 5], s = 5, c = 'r')
     
-    plt.scatter(data_test[t_ids_test, 3], data_test[t_ids_test, 4], s = 5)
-    plt.scatter(data_test[t_ids_test, 3], data_test[t_ids_test, 5], s = 5)
+    # plt.scatter(data_test[t_ids_test, 3], data_test[t_ids_test, 4], s = 5)
+    # plt.scatter(data_test[t_ids_test, 3], data_test[t_ids_test, 5], s = 5)
     
-
-    ## # plt.scatter(outputs[:, 0], outputs[:, 1], s = 5, c = 'green')
-    # print('shape of outputs', outputs.shape)
     plt.scatter(x_inputs, outputs[:, 0], marker = '*', s = 5)
     plt.scatter(x_inputs, outputs[:, 1], marker = '*', s = 5)
+    
     plt.title(f'timestep = {timestep}')
-    plt.legend(['train_u', 'train_v', 'tar_u', 'tar_v', 'test_u', 'test_v'], loc = 'lower left')
+    plt.legend(['train_u', 'train_v', 'pred_u', 'pred_v'], loc = 'lower left')
     
     plt.xlim(-5, 5)
     plt.ylim(-4.5, 3)
@@ -86,9 +74,3 @@ for _id, timestep in tqdm.tqdm(enumerate(timesteps)):
     plt.close()
 
 make_gif('./test_model/', 'solution_test.gif')
-    # plt.show()
-
-# for i, x in enumerate(range(data.shape[0])):
-#     print(i, data[x, 2], data[x, 1], data[x, 0])
-
-    

tools/losses.py
@@ -13,7 +13,7 @@ def loss_0(data, model):
 
     _input = torch.stack((data[:, 1], torch.zeros_like(data[:, 1]))).T
 
-    h_init_pred = model(_input)
+    h_init_pred = model(x = _input[:, 0], t = _input[:, 1])
     h_init_tar = data[:, [2, 3]]
     MSE_0 = criterion(h_init_pred, h_init_tar)
 
@@ -24,8 +24,8 @@ def loss_b(data, model):
     left_input = torch.stack((torch.ones_like(data[:, 1])*(-5), data[:, 1])).T.requires_grad_()
     right_input = torch.stack((torch.ones_like(data[:, 1])*(5), data[:, 1])).T.requires_grad_()
 
-    left_output = model(left_input)
-    right_output = model(right_input)
+    left_output = model(x = left_input[:, 0], t = left_input[:, 1])
+    right_output = model(x = right_input[:, 0], t = right_input[:, 1])
 
     # a, b = left_output.shape
 
@@ -44,8 +44,12 @@ def loss_b(data, model):
 
 
 def loss_f(data, model, equation):
+    x = data[:, 3]
+    t = data[:, 2]
 
-    u_data = model(data[:, [3, 2]]) # x and then t.
+    # u_data = model(data[:, [3, 2]]) # x and then t.
+
+    u_data = model(x, t)
 
     # a, b = u_data.shape
     # u_grad = torch.diagonal(torch.autograd.functional.jacobian(model))
@@ -64,6 +68,6 @@ def loss_f(data, model, equation):
 
     MSE_f = f.mean() # criterion_f(f, torch.zeros_like(f))
 
-    print('mse_f', MSE_f)
+    # print('mse_f', MSE_f)
 
     return MSE_f

tools/model.py
@@ -29,11 +29,13 @@ class NN(torch.nn.Module):
         for l in self.layers:
             torch.nn.init.xavier_uniform_(l.weight)
 
-    def forward(self, x):
+    def forward(self, x, t):
+        d = torch.stack([x, t], dim = 1)
+        # print('shape of input', d.shape)
         for l in self.layers[:-1]:
-            x = self.activation(l(x))
-        x = self.layers[-1](x)
-        return x
+            d = self.activation(l(d))
+        d = self.layers[-1](d)
+        return d
 
     def load_weights(self, weights_dir):
         if os.path.isfile(weights_dir):
@@ -47,68 +49,91 @@ class NN(torch.nn.Module):
 
     def dx(self, data):
         data.requires_grad_()
-        # a, b = self.batch_size, self.output_dim
+        x = data[:, 0]
+        t = data[:, 1]
 
-        # output_grad =  torch.autograd.functional.jacobian(self, data).view((a*b, a*b))
-        # output_dx = torch.stack((output_grad[:self.batch_size, :self.batch_size].diag(), output_grad[self.batch_size:, :self.batch_size].diag())).T
-        ### print('dx requires grad ?', data.requires_grad)
-        output= self.forward(data)
-        u_dx = torch.autograd.grad(output[:, [0]], data, torch.ones((data.shape[0], 1)).to(self.device), create_graph = True, retain_graph = True)[0]
-        v_dx = torch.autograd.grad(output[:, [1]], data, torch.ones((data.shape[0], 1)).to(self.device), create_graph = True, retain_graph = True)[0]
+        u = self.real(x, t)
+        v = self.imag(x, t)
+    
+        u_dx = torch.autograd.grad(u, x, torch.ones_like(u), create_graph = True, retain_graph = True)[0]
+        v_dx = torch.autograd.grad(v, x, torch.ones_like(v), create_graph = True, retain_graph = True)[0]
+        
+        output_dx = torch.stack((u_dx, v_dx), dim = 1)
 
-        output_dx = torch.stack((u_dx[:, 0], v_dx[:, 0])).T
 
         return output_dx
 
     def dt(self, data):
         data.requires_grad_()
+
+        x = data[:, 0]
+        t = data[:, 1]
+
+        u = self.real(x, t)
+        v = self.imag(x, t)
+
+        
+
         #### print('dt requires grad ?', data.requires_grad)
         # a, b = self.batch_size, self.output_dim
 
         # output_grad =  torch.autograd.functional.jacobian(self, data).view((a*b, a*b))
         # output_dt = torch.stack((output_grad[:self.batch_size, self.batch_size:].diag(), output_grad[self.batch_size:, self.batch_size:].diag())).T
         
-        output= self.forward(data)
-        u_dt = torch.autograd.grad(output[:, [0]], data, torch.ones((data.shape[0], 1)).to(self.device),create_graph = True, retain_graph = True)[0]
-        v_dt = torch.autograd.grad(output[:, [1]], data, torch.ones((data.shape[0], 1)).to(self.device),create_graph = True, retain_graph = True)[0]
+        u_dt = torch.autograd.grad(u, t, torch.ones_like(u), create_graph = True, retain_graph = True)[0]
+        v_dt = torch.autograd.grad(v, t, torch.ones_like(v), create_graph = True, retain_graph = True)[0]
 
-        output_dt = torch.stack((u_dt[:, 1], v_dt[:, 1])).T
+        output_dt = torch.stack((u_dt, v_dt), dim = 1)
 
         return output_dt
 
     def dx2(self, data):
         data.requires_grad_()
-        output = self.forward(data)
-        du = torch.autograd.grad(output[:, [0]], data, torch.ones((data.shape[0], 1)).to(self.device),create_graph = True, retain_graph = True)[0]
-        dv = torch.autograd.grad(output[:, [1]], data, torch.ones((data.shape[0], 1)).to(self.device),create_graph = True, retain_graph = True)[0]
-        # du.requires_grad_()
-        # dv.requires_grad_()
-        #### print('d2u', du.requires_grad, data.requires_grad)
+        
+        x = data[:, 0]
+        t = data[:, 1]
 
-        d2u = torch.autograd.grad(du, data, torch.ones_like(data).to(self.device),create_graph = True, retain_graph = True)[0]
-        d2v = torch.autograd.grad(dv, data, torch.ones_like(data).to(self.device),create_graph = True, retain_graph = True)[0]
+        u = self.real(x, t)
+        v = self.imag(x, t)
 
+        u_dx = torch.autograd.grad(u, x, torch.ones_like(u), create_graph = True, retain_graph = True)[0]
+        v_dx = torch.autograd.grad(v, x, torch.ones_like(v), create_graph = True, retain_graph = True)[0]
+        
+        u_dx2 = torch.autograd.grad(u_dx, x, torch.ones_like(u_dx), create_graph = True, retain_graph = True)[0]
+        v_dx2 = torch.autograd.grad(v_dx, x, torch.ones_like(v_dx), create_graph = True, retain_graph = True)[0]
+
+
+       
         # output_dx2 = d2u[:, [0]]
-        output_dx2 = torch.stack((d2u[:, 0], d2v[:, 0])).T
+        output_dx2 = torch.stack((u_dx2, v_dx2), dim = 1)
 
         return output_dx2
     
     def dt2(self, data):
         data.requires_grad_()
-        output = self.forward(data)
-        du = torch.autograd.grad(output[:, [0]], data, torch.ones((data.shape[0], 1)).to(self.device),create_graph = True, retain_graph = True)[0]
-        dv = torch.autograd.grad(output[:, [1]], data, torch.ones((data.shape[0], 1)).to(self.device),create_graph = True, retain_graph = True)[0]
-        # du.requires_grad_()
-        # dv.requires_grad_()
-        ### print('d2u', du.requires_grad, data.requires_grad)
+        
 
-        d2u = torch.autograd.grad(du, data, torch.ones_like(data).to(self.device),create_graph = True, retain_graph = True)[0]
-        d2v = torch.autograd.grad(dv, data, torch.ones_like(data).to(self.device),create_graph = True, retain_graph = True)[0]
+        x = data[:, 0]
+        t = data[:, 1]
 
-        # output_dx2 = d2u[:, [0]]
-        output_dt2 = torch.stack((d2u[:, 1], d2v[:, 1])).T
+        u = self.real(x, t)
+        v = self.imag(x, t)
+
+        u_dt = torch.autograd.grad(u, t, torch.ones_like(u), create_graph = True, retain_graph = True)[0]
+        v_dt = torch.autograd.grad(v, t, torch.ones_like(v), create_graph = True, retain_graph = True)[0]
+
+        u_dt2 = torch.autograd.grad(u_dt, t, torch.ones_like(u_dt), create_graph = True, retain_graph = True)[0]
+        v_dt2 = torch.autograd.grad(v_dt, t, torch.ones_like(v_dt), create_graph = True, retain_graph = True)[0]
+
+        output_dt2 = torch.stack((u_dt2, v_dt2), dim = 1)
 
         return output_dt2
+    
+    def real(self, x, t):
+        return self.forward(x, t)[:, 0]
+
+    def imag(self, x, t):
+        return self.forward(x, t)[:, 1]
 
     # def dt2(self, data):
     #     data.requires_grad_()
@@ -145,6 +170,7 @@ class NN(torch.nn.Module):
     #     output_dx2 = torch.stack((output_u_dx2, output_v_dx2)).T
 
     #     return output_dx2
+
 
 def Real(model, j):
     def real_part(data):

tools/trainer.py
@@ -36,6 +36,8 @@ def fit(model, dataloader, optimizer, scheduler, type_ = 'LBFGS'):
 
                 _loss = l_0 + l_b + l_f  
 
+                # print(f'l_0 : {l_0} | l_b : {l_b} | l_f {l_f}')
+
                 _loss.backward()
                 return _loss
 
@@ -126,7 +128,7 @@ def train(model, train_loader, val_loader, optimizer, scheduler, n_epochs, weigh
             }
             torch.save(save_dict, weights_dir)
             min_loss = val_epoch_loss
-        if epoch%5 == 0:
+        if epoch%2 == 0:
             save_dict = {
                 'epoch' : epoch,
                 'lr': optimizer.param_groups[0]['lr'],